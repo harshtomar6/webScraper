f6c149c32468e6162f4c3031456b31ed239d69f8
Harsh Tomar <harshtomar6@gmail.com>
2017-09-24 01:23:38 +0530

    Added test

--- api/routes/homeRoute.js
+++ api/routes/homeRoute.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
   res.sendFile('../../public/index.html');
 })
 
+router.get('/test', (req, res) => {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.send(ip)
+})
+
 module.exports = router;
